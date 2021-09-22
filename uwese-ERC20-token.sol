d2ab2f26f4bbfa51d60c6ae0e855e54389d5be8f
Gino <gino.osahon@gmail.com>
2021-09-22 15:38:59 +0100

    updated my token contract

--- uwese-ERC20-token.sol
+++ uwese-ERC20-token.sol
@@ -3,8 +3,10 @@ pragma solidity ^0.8.6;
 
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol";
 
-contract MyToken is ERC20 {
-    constructor(uint256 initialSupply) ERC20("Uwese Token", "UWA")  {
-        super._mint(msg.sender, initialSupply);
-    }
+abstract contract MyToken is ERC20 {
+    
+	constructor(uint256 initialSupply, address busOwnerAddress, string memory _name, string memory _symbol) ERC20(_name, _symbol) {
+		super._mint(busOwnerAddress, initialSupply);
+	}
+	
 }
